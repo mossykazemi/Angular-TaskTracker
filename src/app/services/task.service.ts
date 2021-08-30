cfb7b91f7e1e8411c102b65e674f6029285650c5
Mostafa Kazemi <m.kazemi.512@gmail.com>
2021-08-30 20:29:01 +0430

    complete Add-Task

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -3,11 +3,11 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, observable } from 'rxjs';
 import { Task } from '../Task';
 
-const httpOptions={
-  headers:new HttpHeaders({
-    'Content-Type':'application/json'
-  })
-}
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
 
 @Injectable({
   providedIn: 'root',
@@ -26,8 +26,12 @@ export class TaskService {
     return this.http.delete<Task>(url);
   }
 
-  updateTaskReminder(task:Task):Observable<Task>{
+  updateTaskReminder(task: Task): Observable<Task> {
     const url = `${this.apiUrl}/${task.id}`;
-    return this.http.put<Task>(url,task,httpOptions);
+    return this.http.put<Task>(url, task, httpOptions);
+  }
+
+  addTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.apiUrl, task, httpOptions);
   }
 }

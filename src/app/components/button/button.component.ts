da589f814b95c1c3814eb6414a69f81ca05c14a6
Mostafa Kazemi <m.kazemi.512@gmail.com>
2021-08-30 16:15:01 +0430

    Added task Mock +Task Interface +modified button for reuse

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  styleUrls: ['./button.component.css'],
 })
 export class ButtonComponent implements OnInit {
+  @Input() text: any;
+  @Input() color: any;
+  @Output() btnClick = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onClick() {
+    this.btnClick.emit();
   }
-
 }

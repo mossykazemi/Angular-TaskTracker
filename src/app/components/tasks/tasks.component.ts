e7296be16bdfdb15c012ac7913ef30d238ac9117
Mostafa Kazemi <m.kazemi.512@gmail.com>
2021-08-30 16:25:42 +0430

    task-item created

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from '../../Task';
+import { TASKS } from '../../mock-tasks';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.css']
+  styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent implements OnInit {
+  tasks: Task[] = TASKS;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
